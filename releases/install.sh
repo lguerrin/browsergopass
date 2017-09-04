18cee2b6557e04d83e190ce55869781ef3a6ae90
Loïc Guerrin <guerrin@fullsix.com>
2017-09-04 16:49:08 +0200

    Add chromium support

--- releases/install.sh
+++ releases/install.sh
@@ -4,26 +4,57 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 APP_NAME="com.lguerrin.browsergopass"
 
 OS=`uname -s`
+NC='\033[0m'
+
+function printGreen {
+    echo -e "\033[32m$1$NC"
+}
+
+function printLightGreen {
+    echo -e "\033[92m$1$NC"
+}
+
+function printRed {
+    echo -e "\033[31m$1$NC"
+}
+
+function printYellow {
+    echo -e "\033[33m$1$NC"
+}
+
+function succesfulInstall {
+    printLightGreen "\t\u2713 install OK"
+}
+
+function addApp {
+    printGreen "\tAdd app..."
+}
+
 
 function detectApp {
+    echo "Checking $1..."
     INSTALLED=0
     case ${OS} in
         Linux)
-            if [ -z `hash $1` ]; then
-                INSTALLED=0
-            else
-                INSTALLED=1
-            fi
+
+            hash $1 > /dev/null 2>&1
             ;;
         Darwin)
-            osascript -e "id of application \"$1\""  > /dev/null
-            INSTALLED=$?
+            osascript -e "id of application \"$1\""  > /dev/null 2>&1
             ;;
         *)
-            echo "Cannot detect app on $OS"
+            printRed "\u2718Cannot detect app on $OS"
             exit 1
         ;;
     esac
+
+    INSTALLED=$?
+
+    if [ $INSTALLED -eq 0 ]; then
+        printYellow "\tInstalled"
+    else
+        printYellow "\t\u2718 Not found"
+    fi
 }
 
 case ${OS} in
@@ -32,6 +63,9 @@ case ${OS} in
         CHROME_APP="google-chrome"
         TARGET_DIR_FIREFOX="$HOME/.mozilla/native-messaging-hosts/"
         FIREFOX_APP="firefox"
+        TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
+        CHROMIUM_APP="chromium-browser"
+
         HOST_FILE="$DIR/browsergopass"
         ;;
     Darwin)
@@ -42,7 +76,7 @@ case ${OS} in
         HOST_FILE="$DIR/browsergopass-wrapper"
         ;;
     *)
-        echo "Cannot install on $OS"
+        printRed "Cannot install on $OS"
         exit 1
     ;;
 esac
@@ -53,28 +87,41 @@ ESCAPED_HOST_FILE=${HOST_FILE////\\/}
 
 detectApp "$FIREFOX_APP"
 if [ $INSTALLED -eq 0 ]; then
-    echo "Detecting Firefox"
     if [ ! -d "$TARGET_DIR_FIREFOX" ]; then
-        "Creates native app folder"
+        printYellow "\tCreates native app folder"
         mkdir -p "$TARGET_DIR_FIREFOX"
     fi
 
-    echo "Installing app for firefox"
+    addApp
     cp "host-firefox.json" "$TARGET_DIR_FIREFOX/$APP_NAME.json"
     sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR_FIREFOX/$APP_NAME.json"
+    succesfulInstall
 fi
 detectApp "$CHROME_APP"
 if [ $INSTALLED -eq 0 ]; then
-     echo "Detecting Chrome"
      if [ ! -d "$TARGET_DIR_CHROME" ]; then
          echo "Creates native app folder"
          mkdir -p "$TARGET_DIR_CHROME"
      fi
 
-     echo "Installing app for chrome"
-    cp "host-chrome.json" "$TARGET_DIR_CHROME/$APP_NAME.json"
-    sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR_CHROME/$APP_NAME.json"
+     addApp
+     cp "host-chrome.json" "$TARGET_DIR_CHROME/$APP_NAME.json"
+     sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR_CHROME/$APP_NAME.json"
+     succesfulInstall
 fi
 
 
-echo "Now, you can add plugins to browsers."
+if [ -n "$CHROMIUM_APP" ]; then
+    detectApp "$CHROMIUM_APP"
+    if [ $INSTALLED -eq 0 ]; then
+        if [ -d "$TARGET_DIR_CHROMIUM" ]; then
+            addApp
+            cp "host-chrome.json" "$TARGET_DIR_CHROMIUM/$APP_NAME.json"
+            sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR_CHROMIUM/$APP_NAME.json"
+            succesfulInstall
+        fi
+    fi
+fi
+
+
+echo -e "\n\e[1;42mNow, you can add plugin to browsers.$NC"
